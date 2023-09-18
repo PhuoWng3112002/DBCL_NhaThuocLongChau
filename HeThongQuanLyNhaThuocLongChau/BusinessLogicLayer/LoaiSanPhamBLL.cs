89fe43f74eaf989cfd957693b5291631224827e8
phương <107473716+PhuoWng3112002@users.noreply.github.com>
2023-09-19 00:30:00 +0700

    Add files via upload

---
+++ HeThongQuanLyNhaThuocLongChau/BusinessLogicLayer/LoaiSanPhamBLL.cs
@@ -0,0 +1,20 @@
+﻿using HeThongQuanLyNhaThuocLongChau.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeThongQuanLyNhaThuocLongChau.BusinessLogicLayer
+{
+    class LoaiSanPhamBLL
+    {
+        LoaiSanPhamDAL loaiSanPhamDAL = new LoaiSanPhamDAL();
+
+        public DataTable findAll()
+        {
+            return loaiSanPhamDAL.findAll();
+        }
+    }
+}
