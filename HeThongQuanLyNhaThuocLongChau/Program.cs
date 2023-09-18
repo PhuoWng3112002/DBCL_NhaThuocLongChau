88102a8b3472d3c8b443795b1d0adabc31ce0aba
phương <107473716+PhuoWng3112002@users.noreply.github.com>
2023-09-19 00:25:47 +0700

    Add files via upload

---
+++ HeThongQuanLyNhaThuocLongChau/Program.cs
@@ -0,0 +1,42 @@
+﻿using HeThongQuanLyNhaThuocLongChau.DataAccessLayer;
+using HeThongQuanLyNhaThuocLongChau.PresentationLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeThongQuanLyNhaThuocLongChau
+{
+    static class Program
+    {
+        public static string constr = "Data Source=KING;Initial Catalog=db_QuanLyHeThongNhaThuocLongChau;Integrated Security=True";
+        
+        public static string maQuyenQuanLy = "Q01";
+        public static string maQuyenBanHang = "Q02";
+        public static string maQuyenKho = "Q03";
+        public static string maQuyenKeToan = "Q04";
+
+        public static string tenNV, maTK, maQuyen, tenTK;
+
+        public static void setSession(string maTK, string maQuyen, string tenTK)
+        {
+            Program.maTK = maTK;
+            Program.maQuyen = maQuyen;
+            Program.tenTK = tenTK;
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new DangNhapGUI());
+
+        }
+    }
+}
